d67873a66de69a609eb90da236cd5c2095777393
samatrhea <s.gerene@rheagroup.com>
2023-01-30 08:36:28 +0100

    [Add] EClass.QueryTypeHierarchy extension method
    [Add] EClass.QuerySpecializations extension method
    [Update] to Handlebars.Net.Helpers version 2.3.13

---
+++ ECoreNetto.Extensions.Tests/ClassExtensionsTestFixture.cs
@@ -0,0 +1,81 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="ClassExtensionsTestFixture.cs" company="RHEA System S.A.">
+// 
+//   Copyright 2017-2023 RHEA System S.A.
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+namespace ECoreNetto.Extensions.Tests
+{
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+
+	using ECoreNetto.Extensions;
+	using ECoreNetto.Resource;
+
+	using NUnit.Framework;
+
+	/// <summary>
+	/// Suite of tests for the <see cref="ModelElementExtensions"/> class
+	/// </summary>
+	[TestFixture]
+	public class ClassExtensionsTestFixture
+	{
+		private EPackage rootPackage;
+
+		[SetUp]
+		public void SetUp()
+		{
+			var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "recipe.ecore");
+			var filePath = Path.GetFullPath(path);
+			var uri = new System.Uri(filePath);
+
+			var resourceSet = new ResourceSet();
+			var resource = resourceSet.CreateResource(uri);
+
+			this.rootPackage = resource.Load(null);
+		}
+
+		[Test]
+		public void Verify_that_QuerySpecializations_returns_the_expected_result()
+		{
+			var tool = this.rootPackage.EClassifiers.OfType<EClass>().Single(x => x.Name == "Tool");
+
+			var specializations =
+				ClassExtensions.QuerySpecializations(tool, this.rootPackage.EClassifiers.OfType<EClass>());
+
+			var expected = new List<EClass>();
+			
+			expected.Add(this.rootPackage.EClassifiers.OfType<EClass>().Single(x => x.Name == "Container"));
+			expected.Add(this.rootPackage.EClassifiers.OfType<EClass>().Single(x => x.Name == "Transformer"));
+			
+			Assert.That(specializations, Is.EquivalentTo(expected));
+		}
+
+		[Test]
+		public void Verify_that_QueryTypeHierarchy_returns_the_expected_result()
+		{
+			var standardAction = this.rootPackage.EClassifiers.OfType<EClass>().Single(x => x.Name == "StandardAction");
+
+			var expectedNames = new List<string> { "Action", "NamedElement" };
+
+			var typeHierarchyNames = standardAction.QueryTypeHierarchy().Select(x => x.Name);
+
+			Assert.That(typeHierarchyNames, Is.EquivalentTo(expectedNames));
+		}
+	}
+}
---
+++ ECoreNetto.Extensions/ClassExtensions.cs
@@ -0,0 +1,74 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="ClassExtensions.cs" company="RHEA System S.A.">
+// 
+//   Copyright 2017-2023 RHEA System S.A.
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+namespace ECoreNetto.Extensions
+{
+	using System.Collections.Generic;
+	using System.Linq;
+
+	/// <summary>
+	/// Extension methods for <see cref="EClass"/> class
+	/// </summary>
+	public static class ClassExtensions
+	{
+		/// <summary>
+		/// Queries for the subject <see cref="EClass"/> which are specializations (sub classes). 
+		/// </summary>
+		/// <param name="class">
+		/// The <see cref="EClass"/> for which the specializations need to be computed
+		/// </param>
+		/// <param name="allClasses">
+		/// The <see cref="IEnumerable{EClass}"/> from which the generalization / specializations are computed
+		/// </param>
+		/// <returns>
+		/// An <see cref="IEnumerable{EClass}"/> that contains the specializations (sub classes) if the subject <see cref="EClass"/>
+		/// </returns>
+		public static IEnumerable<EClass> QuerySpecializations(this EClass @class, IEnumerable<EClass> allClasses)
+		{
+			var result = allClasses
+				.Where(x => x.ESuperTypes.Contains(@class));
+
+			return result;
+		}
+
+		/// <summary>
+		/// Queries the type hierarchy (chain of super classes) of the provided <see cref="EClass"/>
+		/// </summary>
+		/// <param name="class">
+		/// The subject <see cref="EClass"/> for which the type hierarchy (chain of super classes) needs to be computed
+		/// </param>
+		/// <returns>
+		/// An <see cref="IEnumerable{EClass}"/> that contains the chain of superclasses
+		/// </returns>
+		public static IEnumerable<EClass> QueryTypeHierarchy(this EClass @class)
+		{
+			var result = new List<EClass>();
+
+			foreach (var superType in @class.ESuperTypes)
+			{
+				result.Add(superType);
+
+				result.AddRange(superType.QueryTypeHierarchy());
+			}
+
+			return result;
+		}
+	}
+}
