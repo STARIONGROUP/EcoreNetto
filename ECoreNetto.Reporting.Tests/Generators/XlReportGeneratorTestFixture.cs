0c02c8844dc15b1d2cb55ad49ef52a82cf0d33b5
Sam Gerené <s.gerene@stariongroup.eu>
2026-06-01 07:53:45 +0200

    [Fix] write Excel info-sheet ns-uri to its own row so ns-prefix is not overwritten; fixes #30 (#68)

---
+++ ECoreNetto.Reporting.Tests/Generators/XlReportGeneratorTestFixture.cs
@@ -0,0 +1,79 @@
+// -------------------------------------------------------------------------------------------------
+// <copyright file="XlReportGeneratorTestFixture.cs" company="Starion Group S.A">
+//
+//   Copyright 2017-2025 Starion Group S.A.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+namespace ECoreNetto.Reporting.Tests.Generators
+{
+    using System.IO;
+
+    using ClosedXML.Excel;
+
+    using ECoreNetto.Reporting.Generators;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="XlReportGenerator"/> class.
+    /// </summary>
+    [TestFixture]
+    public class XlReportGeneratorTestFixture
+    {
+        private XlReportGenerator generator = null!;
+
+        private FileInfo modelFileInfo = null!;
+
+        private FileInfo reportFileInfo = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.generator = new XlReportGenerator();
+
+            this.modelFileInfo = new FileInfo(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "recipe.ecore"));
+            this.reportFileInfo = new FileInfo(Path.Combine(TestContext.CurrentContext.TestDirectory, "xl-report.xlsx"));
+        }
+
+        [Test]
+        public void Verify_that_the_info_sheet_contains_distinct_ns_prefix_and_ns_uri_rows()
+        {
+            this.generator.GenerateReport(this.modelFileInfo, this.reportFileInfo);
+
+            using var workbook = new XLWorkbook(this.reportFileInfo.FullName);
+            var info = workbook.Worksheet("Model Info");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(info.Cell(4, 1).GetString(), Is.EqualTo("Root Package - ns prefix"));
+                Assert.That(info.Cell(4, 2).GetString(), Is.EqualTo("recipe"));
+                Assert.That(info.Cell(5, 1).GetString(), Is.EqualTo("Root Package - ns uri"));
+                Assert.That(info.Cell(5, 2).GetString(), Is.EqualTo("hu.bme.mit.mdsd.recipe"));
+            });
+        }
+
+        [Test]
+        public void Verify_that_IsValidReportExtension_returns_expected_results()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(this.generator.IsValidReportExtension(new FileInfo("report.xlsx")).Item1, Is.True);
+                Assert.That(this.generator.IsValidReportExtension(new FileInfo("report.txt")).Item1, Is.False);
+            });
+        }
+    }
+}
